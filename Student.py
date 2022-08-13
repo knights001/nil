7fb735b0dd84046ff3569caea4b673434ad0b91c
knights001 <111188664+knights001@users.noreply.github.com>
2022-08-13 20:26:26 +0530

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+
+# In[2]:
+
+
+os.getcwd(
+)
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+pd.read_csv("Student.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
